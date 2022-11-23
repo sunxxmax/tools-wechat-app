f87a1c47978f91b1de3ead50ac339fb88755c230
sunxxmax <sunxxmax@gmail.com>
2022-11-23 21:59:39 +0800

    首页修改

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -16,7 +16,7 @@ const swiperList = [
 
 Page({
     data: {
-        content: "🌈 更多内容开发中，请敬请期待！",
+        content: "🌈 蚁巢欢迎您，更多内容敬请期待！",
         visible: true,
         marquee: {
             speed: 40,
@@ -30,5 +30,61 @@ Page({
         swiperList,
         navigation: { type: 'dots-bar' },
         paginationPosition: 'bottom',
+
+        date: {
+            now: new Date(),
+
+            day: 1,
+            dayText: '无数据',
+            dayPercentage: 0,
+
+            week: 1,
+            weekTexgt: '无数据',
+            weekPercentage: 0,
+
+            month: 1,
+            monthText: '无数据',
+            monthPercentage: 0,
+
+            year: 1970,
+            yearText: '无数据',
+            yearPercentage: 0
+        }
+    },
+    /**
+   * 生命周期函数--监听页面加载
+   */
+    onLoad() {
+        this.dataLoad()
+    },
+
+    dataLoad() {
+        let now = new Date();
+        let date = {
+            now: now,
+
+            day: now.getDate(),
+            dayText: '12/24',
+            dayPercentage: 0,
+
+            week: this.week4Year(now),
+            weekTexgt: '无数据',
+            weekPercentage: 0,
+
+            month: now.getMonth() + 1,
+            monthText: now.getDate() + '/' + 31,
+            monthPercentage: 0,
+
+            year: now.getFullYear(),
+            yearText: now.getMonth() + '/' + now.getDay(),
+            yearPercentage: 0
+        }
+        this.setData({ date: date })
+    },
+
+    week4Year(date: Date) {
+        let fisrtDay = new Date(date.getFullYear(), 0, 1);
+        let d = Math.round((date.valueOf() - fisrtDay.valueOf()) / 86400000);
+        return Math.ceil((d + ((fisrtDay.getDay() + 1) - 1)) / 7);
     }
 })
